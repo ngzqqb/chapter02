cb2f195e7b9319c6056d1eed454f97ce0de4edd0
ngzHappy <zhaixueqiang@hotmail.com>
2019-05-17 09:22:15 +0800

    +checkedLoadQmlFile

--- simple_styled_control/main.cpp
+++ simple_styled_control/main.cpp
@@ -15,11 +15,7 @@ int main(int argc, char ** argv) {
         auto varFullFileName = sstd::autoLocalPath<QUrl>(
             QStringLiteral("appqml/simple_styled_control/main.qml"));
         /*加载Qml文件*/
-        varEngine.load(varFullFileName);
-        /*检查并报错*/
-        if (varEngine.rootObjects().empty()) {
-            return -1;
-        }
+        sstd::checkedLoadQmlFile(&varEngine, &varApplication, varFullFileName);
     }
     return varApplication.exec();
 
